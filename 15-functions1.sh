56f423b2f5205c8e0ef27b729954a6fc9d6f32d5
maahirasheik <maahisheik612@gamil.com>
2025-11-13 00:45:45 +0530

    functions

--- 15-functions1.sh
+++ 15-functions1.sh
@@ -10,23 +10,23 @@ else
 echo "your is running with root access"
 fi
 
-VALIDATE(){
+# VALIDATE(){
 
-if [ $1 -eq 0 ] t
-then
-echo "installing mysql $2....SUCCESS"
-else
-echo "Mysql not installed $2...FALURE"
-exit 1
-fi
-}
+# if [ $1 -eq 0 ] t
+# then
+# echo "installing mysql $2....SUCCESS"
+# else
+# echo "Mysql not installed $2...FALURE"
+# exit 1
+# fi
+# }
 
-dnf list installed mysql
-if [ $? -ne 0 ]
-then 
-echo " mysql is not insatlled...going to insatll"
-dnf install mysql -y
-VALIDATE $? "Mysql"
-else
-echo "mysql is already insatlalled..Nothing to do"
-fi
+# dnf list installed mysql
+# if [ $? -ne 0 ]
+# then 
+# echo " mysql is not insatlled...going to insatll"
+# dnf install mysql -y
+# VALIDATE $? "Mysql"
+# else
+# echo "mysql is already insatlalled..Nothing to do"
+# fi
