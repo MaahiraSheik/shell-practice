ba6e9f17b75e2840b8a1663363f6d6850ddee9c9
maahirasheik <maahisheik612@gamil.com>
2025-11-12 23:26:44 +0530

    installing mysql

--- 13-install-commands.sh
+++ 13-install-commands.sh
@@ -10,8 +10,11 @@ else
 echo "your is running with root access"
 fi
 
+dnf list installed mysql
+if[ $? -ne 0]
+then 
+echo " mysql is not insatlled...going to insatll"
 dnf install mysql -y
-
 if [ $? -eq 0 ]
 then
 echo "installing mysql....SUCCESS"
@@ -19,3 +22,14 @@ else
 echo "Mysql not installed...FALURE"
 exit 1
 fi
+else
+echo "mysql is already insatlalled..Nothing to do"
+fi
+
+# if [ $? -eq 0 ]
+# then
+# echo "installing mysql....SUCCESS"
+# else
+# echo "Mysql not installed...FALURE"
+# exit 1
+# fi
