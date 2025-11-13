3bd749afe13499e6b9eea422638d7c1d2bb74e51
maahirasheik <maahisheik612@gamil.com>
2025-11-13 10:12:32 +0530

    Installing packages

--- 14-installpackages.sh
+++ 14-installpackages.sh
@@ -1,50 +1,30 @@
 #!/bin/bash
 
 USERID=$(id -u)
-if [ $USERID -ne 0 ]
-then
-echo "ERROR: please run with this root access"
-exit 1
+if [ $USERID -ne 0 ]; then
+  echo "ERROR: Please run with root access"
+  exit 1
 else
-echo "your is running with root access"
+  echo "Script is running with root access"
 fi
 
-VALIDATE(){
-if [ $1 -eq 0 ]
-then
-echo "installing mysql....SUCCESS"
-else
-echo "Mysql not installed...FALURE"
-exit 1
-fi
+VALIDATE() {
+  if [ $1 -eq 0 ]; then
+    echo "$2 installation... SUCCESS"
+  else
+    echo "$2 installation... FAILURE"
+    exit 1
+  fi
 }
 
-dnf list installed mysql
-if [ $? -ne 0 ]
-then 
-echo " mysql is not insatlled...going to insatll"
-dnf install mysql -y
-VALIDATE $? "mysql"
-else
-echo "mysql is already insatlalled..Nothing to do"
-fi
-
-dnf list installed python3
-if [ $? -ne 0 ]
-then 
-echo " python3 is not insatlled...going to insatll"
-dnf install python3 -y
-VALIDATE $? "python3"
-else
-echo "python3 is already insatlalled..Nothing to do"
-fi
-
-dnf list installed nginx
-if [ $? -ne 0 ]
-then 
-echo " nginx is not insatlled...going to insatll"
-dnf install nginx -y
-VALIDATE $? "nginx"
-else
-echo "nginx is already insatlalled..Nothing to do"
-fi
+for package in mysql python3 nginx; do
+  echo "Checking if $package is installed..."
+  dnf list installed $package &>/dev/null
+  if [ $? -ne 0 ]; then
+    echo "$package is not installed... Installing now"
+    dnf install -y $package
+    VALIDATE $? "$package"
+  else
+    echo "$package is already installed... Nothing to do"
+  fi
+done
