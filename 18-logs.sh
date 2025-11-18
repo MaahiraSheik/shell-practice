7d62334f766b65f2b2db9c857a0a8c8b56e79d36
maahirasheik <maahisheik612@gamil.com>
2025-11-18 23:08:40 +0530

    logs

--- 18-logs.sh
+++ 18-logs.sh
@@ -7,7 +7,13 @@ LOGFILE="/var/log/myscript.log"
 log() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> "$LOGFILE"
 }
+log_info()  { echo "$(date '+%F %T') [INFO] $1"  >> "$LOGFILE"; }
+log_warn()  { echo "$(date '+%F %T') [WARN] $1"  >> "$LOGFILE"; }
+log_error() { echo "$(date '+%F %T') [ERROR] $1" >> "$LOGFILE"; }
 
+log_info "Service started"
+log_warn "Low disk space"
+log_error "Failed to connect to database"
 log "Script started"
 log "Performing task..."
 log "Script finished"
